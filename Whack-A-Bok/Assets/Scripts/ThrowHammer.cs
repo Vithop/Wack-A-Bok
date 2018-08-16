c9160ef0cddfdec2f3e6e50f3dfba997f734379a
Mathuran <mathuransada@gmail.com>
2018-08-16 15:11:55 -0400

    made ball toss better

--- Whack-A-Bok/Assets/Scripts/ThrowHammer.cs
+++ Whack-A-Bok/Assets/Scripts/ThrowHammer.cs
@@ -2,51 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowHammer : MonoBehaviour {
+public class ThrowHammer : MonoBehaviour
+{
 
     public Camera playerCam;
     private bool throwHammer;
+    public GameObject ballHammer;
     public Rigidbody rb;
     private Vector3 hammerTarget;
+    private float startTapped;
+    private float endTapped;
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         throwHammer = false;
         rb.constraints = RigidbodyConstraints.FreezeAll;
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        throwHammer = false;
+        /*
+        if (throwHammer)
+        {
+            endTapped = Time.time;
+            Debug.Log(endTapped - startTapped);
+            if (Input.touchCount == 1 && (endTapped - startTapped) > 1)
+                MakeHammer();
+        }*/
 
         if (Input.touchCount != 1)
         {
             return;
         }
         var ray = playerCam.ScreenPointToRay(Input.touches[0].position);
+        Touch touch = Input.GetTouch(0);
         var hitInfo = new RaycastHit();
 
-        if(Physics.Raycast(ray, out hitInfo))
+        switch (touch.phase)
         {
-            if (hitInfo.transform.name != "GameField")
-                return;
-            throwHammer = true;
-            hammerTarget = hitInfo.point;
+            case TouchPhase.Began:
+
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    if (hitInfo.transform.name != "GameField")
+                        return;
+                    hammerTarget = hitInfo.point;
+                    throwHammer = true;
+                }
+                break;
+
+            case TouchPhase.Ended:
+                ballHammer = MakeHammer();
+                break;
         }
-	}
+
+
+
+
+
+
+    }
 
     void FixedUpdate()
     {
         if (throwHammer)
         {
+
             rb.constraints = RigidbodyConstraints.None;
-            rb.AddForce( hammerTarget * 100); 
+            rb.AddForce(hammerTarget * 100);
+
+
         }
+    }
+
+
+    GameObject MakeHammer()
+    {
         
+        GameObject newBallHammer = Instantiate(ballHammer /*, hammerPos, hammerRot*/);
+        rb = newBallHammer.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        newBallHammer.transform.SetParent(playerCam.transform);
+
+        var hammerPos = playerCam.transform.position + new Vector3(0.27f,-1.45f,2.37f) ;
+        var hammerRot = playerCam.transform.rotation;
+
+        newBallHammer.transform.position = hammerPos;
+        newBallHammer.transform.rotation = hammerRot;
+        throwHammer = false;
+        return newBallHammer;
     }
 
 }
